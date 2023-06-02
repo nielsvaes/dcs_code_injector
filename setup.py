a1d4af8957d4fdd0559becb4ff99dcb879e4237f
Michal Plichta <mplichta@gmail.com>
2023-06-02 14:12:24 +0200

    add encoding to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", encoding='utf-8') as fh:
     long_description = fh.read()
 
 data_files_to_include = ["*.png", "*.jpg", "*.xml"]
