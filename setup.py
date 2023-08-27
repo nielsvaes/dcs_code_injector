30960bf5375ad978a5c880bfd6c8099a9d021334
Niels Vaes <nielsvaes@gmail.com>
2023-08-27 16:56:39 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ data_files_to_include = ["*.png", "*.jpg", "*.xml"]
 
 setup(
     name='dcs-code-injector',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     package_data={
         "": data_files_to_include,
